97c22fccc2a34e4e9b41a3b83b875d4c68de078b
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-18 12:43:52 -0500

    - Correccion -

--- src/main/java/com/christian/ProyectobootApplication.java
+++ src/main/java/com/christian/ProyectobootApplication.java
@@ -1,4 +1,4 @@
-package com.christian.proyectoboot;
+package com.christian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
