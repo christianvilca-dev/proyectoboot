e117f2acfe7f44ddb1b51246c83e97bf52915115
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-17 23:59:02 -0500

    Inicio

---
+++ src/main/java/com/christian/proyectoboot/ProyectobootApplication.java
@@ -0,0 +1,12 @@
+package com.christian.proyectoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectobootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectobootApplication.class, args);
+	}
+}
