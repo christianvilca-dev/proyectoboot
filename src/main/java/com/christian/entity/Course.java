23d99bb36be9e54930b327a0b036bc5cdc7ed016
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-19 17:20:48 -0500

    Creando entidad Hibernate

---
+++ src/main/java/com/christian/entity/Course.java
@@ -0,0 +1,87 @@
+package com.christian.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ * Hybernate (ORM)
+	Por cada tabla en la bd habra una clase java anotada, con elementos 
+	que haran que mapeen los campos de la tabla automaticamente con los 
+	campos de la clase java
+ */
+
+@Entity
+@Table(name = "Course")
+public class Course {
+
+	@Id
+	@GeneratedValue // Se encarga de saber cual es el siguiente id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="price")
+	private int price;
+	
+	@Column(name="hours")
+	private int hours;
+
+	public Course() {}
+
+	public Course(int id, String name, String description, int price, int hours) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.hours = hours;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+}
