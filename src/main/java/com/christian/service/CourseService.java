404ac2e3416551ee13db94afd3f07f936a5a86bf
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-19 18:34:58 -0500

    Integrando Capas - Controller

--- src/main/java/com/christian/service/CourseService.java
+++ src/main/java/com/christian/service/CourseService.java
@@ -6,7 +6,7 @@ import com.christian.entity.Course;
 
 public interface CourseService {
 
-	public abstract List<Course> listAllCourse();
+	public abstract List<Course> listAllCourses();
 	public abstract Course addCourse(Course course);
 	public abstract int removeCourse(int id);
 	public abstract Course updateCourse(Course course);
