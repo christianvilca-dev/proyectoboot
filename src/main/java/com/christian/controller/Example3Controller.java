9c2384dac14061d70f4c5f8e44fb19f787b80621
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-19 12:43:52 -0500

    Redireccionar 1

--- src/main/java/com/christian/controller/Example3Controller.java
+++ src/main/java/com/christian/controller/Example3Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.christian.model.Person;
 
@@ -16,6 +17,12 @@ public class Example3Controller {
 
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
+	
+	// Primera forma
+	@GetMapping("/")
+	public String redirect() {
+		return "redirect:/example3/showform";
+	}
 	
 	// Muestra el formulario
 	@GetMapping("/showform")
