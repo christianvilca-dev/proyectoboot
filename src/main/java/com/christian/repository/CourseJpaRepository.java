11062af7bd465fb38b03c7de99de71dbc2f1408f
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-19 19:07:17 -0500

    Integrando Capas - Vista 1

--- src/main/java/com/christian/repository/CourseJpaRepository.java
+++ src/main/java/com/christian/repository/CourseJpaRepository.java
@@ -14,11 +14,11 @@ import com.christian.entity.Course;
 public interface CourseJpaRepository extends JpaRepository<Course, Serializable>{
 
 	// Nos hace una query automaticamente y busca por precio
-	public abstract Course findByPrice(int price);
-	
-	public abstract Course findbyPriceAndName(int price, String name);
-	
-	public abstract List<Course> findByNameOrderByHours(String name);
-	
-	public abstract Course findByNameOrPrice(String name, int price);
+//	public abstract Course findByPrice(int price);
+//	
+//	public abstract Course findbyPriceAndName(int price, String name);
+//	
+//	public abstract List<Course> findByNameOrderByHours(String name);
+//	
+//	public abstract Course findByNameOrPrice(String name, int price);
 }
